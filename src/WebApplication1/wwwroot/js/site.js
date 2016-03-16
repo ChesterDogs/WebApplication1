828bce2b17dd016d8c847ef24dfd7e04b2fb892b
ChesterDogs <taltonji@hotmail.com>
2016-03-16 15:55:54 -0400

    Initial Upload

---
+++ src/WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Write your Javascript code.
+
+
+// in support of the $scope topic in tutorial
+// http://docs.asp.net/en/latest/client-side/angular.html
+//
+var personApp = angular.module('myAngularApp', []);
+personApp.controller('personController', ['$scope', function ($scope) {
+    $scope.jsName = 'Mary Jane';
+}]);
+//
+
+
+// in support of the Modules topic in tutorial
+// http://docs.asp.net/en/latest/client-side/angular.html
+// module
+var personApp = angular.module('myAngularApp', []);
+
+// controller
+personApp.controller('personController', function ($scope) {
+    $scope.jsFirstName = "Mary";
+    $scope.jsLastName = "Jane"
+});
